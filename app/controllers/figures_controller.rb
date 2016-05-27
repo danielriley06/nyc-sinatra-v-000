65f2117f248cfc3cb7880b671a28754592148418
danielriley06 <driley3@gmail.com>
2016-05-27 15:28:32 -0400

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,50 @@
+class FiguresController < ApplicationController
+
+  set :views, Proc.new { File.join(root, "../views/figures") }
+
+  get "/figures" do
+    @figures = Figure.all
+    erb :index
+  end
+
+  get "/figures/new" do
+    erb :new
+  end
+
+  get "/figures/:id" do
+    @figure = Figure.find(params[:id])
+    erb :show
+  end
+
+  get "/figures/:id/edit" do
+    @figure = Figure.find(params[:id])
+    erb :edit
+  end
+
+  post "/figures/new" do
+    @figure = Figure.create(params[:figure])
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+    redirect "/figures"
+  end
+
+  post '/figures/:id' do
+      @figure = Figure.find_by(id: params[:id])
+      @figure.titles.clear
+      @figure.landmarks.clear
+      @figure.update(name: params[:figure][:name])
+
+      create_or_update_landmark(params[:figure][:landmark_ids], params[:landmark][:name])
+      create_or_update_title(params[:figure][:title_ids], params[:title][:name])
+
+      @figure.save
+
+      redirect "/figures/#{@figure.id}"
+    end
+
+end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,35 @@
 class LandmarksController < ApplicationController
 
+  set :views, Proc.new { File.join(root, "../views/landmarks") }
+
+  get "/landmarks" do
+    erb :index
+  end
+
+  get "/landmarks/new" do
+    erb :new
+  end
+
+  get "/landmarks/:id" do
+    @landmark = Landmark.find(params[:id])
+    erb :show
+  end
+
+  get "/landmarks/:id/edit" do
+    @landmark = Landmark.find(params[:id])
+    erb :edit
+  end
+
+  post "/landmarks" do
+    @landmark = Landmark.create(params[:landmark])
+    redirect "/landmarks"
+  end
+
+  post "/landmarks/:id" do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params[:landmark])
+    @landmark.save
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
 end
